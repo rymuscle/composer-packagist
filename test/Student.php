dc88df778d0ce5a2adf7f5de982036107060ac0e
renyimin <renyimin@haodai.net>
2018-02-03 15:58:31 +0800

    test

---
+++ test/Student.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: renyimin
+ * Date: 2018/2/3
+ * Time: 下午3:56
+ */
+namespace Lant\Test;
+
+class Student
+{
+    public function say()
+    {
+        echo "I am a student";
+    }
+}
