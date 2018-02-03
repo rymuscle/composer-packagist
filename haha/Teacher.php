c73993c9e5e61753a3c66f3d17fc879f887facdf
renyimin <renyimin@haodai.net>
2018-02-03 17:09:38 +0800

    haj

---
+++ haha/Teacher.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: renyimin
+ * Date: 2018/2/3
+ * Time: 下午5:01
+ */
+namespace Haha;
+class Teacher
+{
+    public function say()
+    {
+        echo "I am a teacher!!";
+    }
+}
